bd0e0f20850d7e6ab738b37510684759e4c697e5
Vujadin <vujke456@gmail.com>
2023-02-13 11:52:36 +0100

    fixing bugs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { nanoid } from "nanoid";
 import Button from "./Button";
 
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { AiFillCloseCircle, AiOutlineCloseCircle } from "react-icons/ai";
 
 export default function Modal({ setIsModalOn }) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div className="modal-container">
-      <div onClick={() => setIsModalOn(false)} className="modal-close-btn">
-        <AiOutlineCloseCircle />
+      <div
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onClick={() => setIsModalOn(false)}
+        className="modal-close-btn"
+      >
+        {isHovered ? <AiFillCloseCircle /> : <AiOutlineCloseCircle />}
       </div>
       <p className="modal-text">Enter your note</p>
     </div>
